a4c92acc04ab7a227497b5ecd2f0a274707da7c9
Kevin Jolley <serrowpls@gmail.com>
2018-10-28 10:01:52 -0500

    auth set-up but not working in code

--- 10-28/README.md
+++ 10-28/README.md
@@ -5,3 +5,16 @@
 **Used:** HTML, LESS, JQuery
 
 Will be refactored into React/Redux with Styled-Components at a later project date.
+
+### API Notes
+
+**Pull**
+
+- Image (Small/Large)
+- Achievements
+- Titles
+- Current PvP Rating
+- Highest PvP Rating (Weekly)
+- Highest PvP Rating (All-Time)
+- Gear Score
+- Alts / Main
--- 10-28/js/main.js
+++ 10-28/js/main.js
@@ -1,3 +1,7 @@
+// ***
+// Modal
+// ***
+
 var modal = document.querySelector('.modal');
 var trigger = document.querySelector('.trigger_login');
 var closeButton = document.querySelector('.close-button');
@@ -15,3 +19,32 @@ function windowOnClick(event) {
 trigger.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', toggleModal);
 window.addEventListener('click', windowOnClick);
+
+// ***
+// API Get
+// ***
+
+var httpRequest = new XMLHttpRequest();
+
+httpRequest.addEventListener('load', clipsLoaded);
+httpRequest.open(
+  'GET',
+  "https://us.api.blizzard.com/wow/character/mal'ganis/serrow"
+);
+httpRequest.setRequestHeader('Client-ID', 'e61b97e125db4c658703144157c9c6aa');
+httpRequest.setRequestHeader('token', `process.env.AUTH_TOKEN`);
+httpRequest.send();
+console.log(httpRequest.responseText);
+
+function clipsLoaded() {
+  // let clipsDisplay = document.getElementById('clips-display');
+  // clipList = JSON.parse(httpRequest.responseText);
+  // clipList.data.map((clip, i) => {
+  //   clipItem = document.createElement('div');
+  //   clipItem.innerHTML =
+  //     '<iframe src="https://clips.twitch.tv/embed?clip=' +
+  //     clip.id +
+  //     '&autoplay=false" height="480" width="854" frameborder="0" scrolling="false" allowfullscreen="true" class="replay_frame"></iframe>';
+  //   clipsDisplay.appendChild(clipItem);
+  // });
+}
