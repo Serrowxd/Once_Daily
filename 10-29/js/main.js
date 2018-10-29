2d6da488924f0e0b5d78a7ad1d36b8d301a91be9
Kevin Jolley <serrowpls@gmail.com>
2018-10-29 11:25:03 -0500

    state nav done

--- 10-29/js/main.js
+++ 10-29/js/main.js
@@ -2,20 +2,48 @@
 // Transition
 // ***
 
-//
 $('.1025_click').on('click', () => {
   hideAll();
   $('#ten25').show();
+});
+
+$('.1026_click').on('click', () => {
+  hideAll();
+  $('#ten25').show();
+});
+
+$('.1027_click').on('click', () => {
+  hideAll();
+  $('#ten25').show();
+});
+
+$('.1028_click').on('click', () => {
+  hideAll();
+  $('#ten25').show();
+});
+
+$('.1029_click').on('click', () => {
+  hideAll();
+  $('#ten25').show();
+});
+
+$('button').on('click', () => {
+  hideAll();
+  $('#one').show();
 });
 
 // Hide All Sections
 hideAll = () => {
   $('#one').hide();
   $('#ten25').hide();
+  $('#ten26').hide();
+  $('#ten27').hide();
+  $('#ten28').hide();
+  $('#ten29').hide();
 };
 
 // Page Load
 $(document).ready(() => {
   hideAll();
-  $('#ten25').show();
+  $('#one').show();
 });
