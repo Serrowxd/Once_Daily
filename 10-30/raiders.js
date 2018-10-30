bf2c0eb0aaa7756b6ab363cc65211a1db8e6d653
Kevin Jolley <serrowpls@gmail.com>
2018-10-30 13:12:06 -0500

    cross-browser support

---
+++ 10-30/raiders.js
@@ -0,0 +1,12 @@
+const Raiders = [
+  {
+    name: 'Mariels',
+    class: 'Death Knight',
+    color: 'red',
+    raiderio: 'https://raider.io/characters/us/area-52/Mariels',
+    logs: 'https://www.warcraftlogs.com/character/us/area-52/mariels',
+    armory: 'https://worldofwarcraft.com/en-us/character/area-52/Mariels',
+    imgb:
+      'https://render-us.worldofwarcraft.com/character/area-52/222/166527454-main.jpg',
+  },
+];
